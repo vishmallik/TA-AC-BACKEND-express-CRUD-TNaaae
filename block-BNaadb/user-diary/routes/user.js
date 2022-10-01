9623771600d27e3e387f930e63bd36c272848005
visawjeet mallik <vishmallik@hotmail.com>
2022-10-01 18:49:44 +0530

    delete ops

---
+++ block-BNaadb/user-diary/routes/user.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+router.get("/new", (req, res) => {
+  res.render("userForm");
+});
+
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  User.create(req.body, (err, createdUser) => {
+    if (err) {
+      res.redirect("/users/new");
+    }
+    res.redirect("/");
+  });
+});
+
+router.get("/", (req, res, next) => {
+  User.find({}, (err, usersList) => {
+    if (err) return next(err);
+    res.render("users", { usersList });
+  });
+});
+
+router.get("/:id", (req, res, next) => {
+  let id = req.params.id;
+  User.findById(id, (err, user) => {
+    if (err) return next(err);
+    res.render("singleUser", { user });
+  });
+});
+
+router.get("/:id/edit", (req, res, next) => {
+  let id = req.params.id;
+  User.findById(id, (err, user) => {
+    if (err) return next(err);
+    res.render("updateUser", { user });
+  });
+});
+
+router.post("/:id", (req, res, next) => {
+  let id = req.params.id;
+  User.findByIdAndUpdate(id, req.body, (err) => {
+    if (err) return next(err);
+    res.redirect("/users");
+  });
+});
+
+router.get("/:id/delete", (req, res, next) => {
+  let id = req.params.id;
+  User.findByIdAndDelete(id, (err) => {
+    if (err) return next(err);
+    res.redirect("/users");
+  });
+});
+
+module.exports = router;
